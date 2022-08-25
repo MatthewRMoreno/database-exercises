3c6e163194e01e4f885005b462f13f157762bf35
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-08-25 11:37:17 -0500

    Complete: Added the albums table to Adminer

---
+++ albums_migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50) NOT NULL,
+                        name  VARCHAR(100) NOT NULL,
+                        release_date INT NOT NULL,
+                        sales FLOAT NOT NULL,
+                        genre VARCHAR(100) NOT NULL,
+                        PRIMARY KEY (id)
+);
