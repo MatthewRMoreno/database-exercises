5f65abf00b140b2a031ec8db7dcc519797c6b3fa
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-09-02 13:34:11 -0500

    Complete: Finished join exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -18,13 +18,13 @@ INNER JOIN dept_manager as dm ON dm.emp_no = e.emp_no
 INNER JOIN departments as d ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
 -- employees working in customer service department title
-SELECT title, count(titles.emp_no)
+SELECT title, count(*) as total
 FROM titles
          join dept_emp de on titles.emp_no = de.emp_no
-where titles.to_date = '9999-01-01'
+where de.to_date = '9999-01-01' AND titles.to_date = '9999-01-01'
   AND dept_no = 'd009'
 GROUP BY title
-ORDER BY count(titles.emp_no) desc;
+ORDER BY count(*) desc;
 -- current salary of managers
 SELECT distinct d.dept_name AS Department_Name,
                 CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager,
